368baa47c24caee207547b39166c434e0333a348
AaronJaig <128724339+AaronJaig@users.noreply.github.com>
2025-09-10 22:34:08 +0800

    Redesign home page and update navbar styles
    Refactored the Home page layout with new animated sections, updated personal introduction, and added animated role titles. Introduced new CSS for modern styling and animations. Updated Navbar to highlight active links and changed branding to 'Portfolio'. Added Boxicons CDN for social icons.

--- Portfolio/src/components/Navbar.jsx
+++ Portfolio/src/components/Navbar.jsx
@@ -1,9 +1,13 @@
+import Portfolio from "../page/Portfolio";
+import { useState } from "react";
+
 function Navbar() {
+  const [active, setActive] = useState("active");
   return (
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <div class="container-fluid">
-        <a class="navbar-brand" href="#home">
-          Personal Website
+        <a class="logo navbar-brand" href="#home">
+          Portfolio
         </a>
         <button
           class="navbar-toggler"
@@ -19,12 +23,23 @@ function Navbar() {
         <div class="collapse navbar-collapse" id="navbarNav">
           <ul class="navbar-nav ms-auto">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#home">
+              <a
+                class={`homeBtn nav-link${
+                  window.location.pathname === "/" ? " active" : ""
+                }`}
+                aria-current="page"
+                href={"/"}
+              >
                 Home
               </a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#about">
+              <a
+                class={`portfolioBtn nav-link${
+                  window.location.pathname === "/portfolio" ? " active" : ""
+                }`}
+                href={"/portfolio"}
+              >
                 Portfolio
               </a>
             </li>
--- Portfolio/src/page/Home.jsx
+++ Portfolio/src/page/Home.jsx
@@ -7,107 +7,73 @@ function Home() {
   return (
     <div>
       {/* //start of home */}
-      <div className=" my-4 mx-auto m-5">
-        <div className="row g-0 m-5">
-          <div className="col-md-7 p-5 rounded d-flex flex-column bg-light justify-content-center text-end">
-            <div className="p-5">
-              <h1 style={{ fontSize: 100 }}>Hi, I am</h1>
-              <h2 style={{ fontSize: 120 }}> Jai Gelle J. Aaron</h2>
+      <div className=" my-4 mx-auto mr-5 ml-5 mb-0">
+        <div className="animation">
+          <div className="bar" style={{ "--i": 6 }}></div>
+          <div className="bar" style={{ "--i": 5 }}></div>
+          <div className="bar" style={{ "--i": 4 }}></div>
+          <div className="bar" style={{ "--i": 3 }}></div>
+          <div className="bar" style={{ "--i": 2 }}></div>
+          <div className="bar" style={{ "--i": 1 }}></div>
+        </div>
+        <div className="row g-0 ">
+          <div className="homeContent col-md-7 p-5 rounded d-flex flex-column  justify-content-center text-end">
+            <div className="p-5 homeDetails">
+              <h1>Hi, I am </h1>
+              <h1> Jai Gelle J. Aaron</h1>
+              <h2>
+                {" "}
+                <span style={{ "--i": 4 }} data-text="Learner">
+                  Learner
+                </span>{" "}
+                <span style={{ "--i": 3 }} data-text="Educator">
+                  Educator
+                </span>{" "}
+                <span style={{ "--i": 2 }} data-text="Enthusiast">
+                  Enthusiast
+                </span>{" "}
+                <span style={{ "--i": 1 }} data-text="Student">
+                  Student
+                </span>
+              </h2>
               <h4>A Graduate in Bachelor of Science in Computer Science</h4>
               <h4>at Bohol Island State University - Clarin Campus</h4>
+              <div className="btn-sci">
+                <a href="#" className="btnTalk">
+                  Let's Talk
+                </a>
+                <div className="sci">
+                  <a href="#">
+                    <i className="bx bxl-github"></i>
+                  </a>
+                  <a href="#">
+                    <i className="bx bxl-linkedin"></i>
+                  </a>
+                  <a href="#">
+                    <i className="bx bxl-twitter"></i>
+                  </a>
+                  <a href="#">
+                    <i className="bx bxl-youtube"></i>
+                  </a>
+                </div>
+              </div>
             </div>
           </div>{" "}
-          <div className="col-md-5 d-flex align-items-center p-3 rounded bg-dark">
-            <img src={meImg} className="img-fluid rounded" alt="Card image" />
+          <div className="homeImage col-md-5 d-flex align-items-center  rounded ">
+            <div className="image-box">
+              <div className="image-item">
+                {" "}
+                <img
+                  src={meImg}
+                  className="img-fluid rounded"
+                  alt="Card image"
+                />
+              </div>
+            </div>
           </div>
         </div>
       </div>
       {/* //end of home //start of about me */}
-      <div className="my-4 mx-auto m-5 bg-light mt-5">
-        <div className="row g-0 m-5">
-          <div className="col-md-6">
-            <div className="align-items-center p-3 rounded">
-              <img src={image1} alt="" className="img-fluid rounded" />
-            </div>
-            <div className="align-items-center p-3 rounded">
-              <img src={image3} alt="" className="img-fluid rounded" />
-            </div>
-          </div>
-          <div className="col-md-6">
-            <div className="p-5 rounded d-flex flex-column bg-light justify-content-center ">
-              <h2 style={{ fontSize: 50 }}>About Me</h2>
-              <div className="text-justify">
-                <p style={{ fontSize: 25, textAlign: "justify" }}>
-                  In the beginning, my aspirations were vague, shaped more by
-                  curiosity than by a concrete plan. I was a seeker, eager to
-                  explore but unsure of what I was searching for. I dabbled in
-                  various pursuits—some academic, some creative, others
-                  practical—each offering a glimpse of potential but none
-                  feeling like home. I studied, worked odd jobs, and chased
-                  opportunities that seemed promising, yet a sense of purpose
-                  eluded me. The uncertainty was daunting, but it also fueled my
-                  determination to keep moving forward, trusting that clarity
-                  would come with time.
-                  <br />
-                </p>
-              </div>
-              <div className="text-justify">
-                {" "}
-                <p style={{ fontSize: 25, textAlign: "justify" }}>
-                  The turning point arrived unexpectedly, as life’s pivotal
-                  moments often do. My Instructor during my College years
-                  suggested I take on a Contract of Service teaching role. I
-                  hesitated at first—teaching wasn’t something I’d ever
-                  considered seriously. I imagined it as a temporary gig, a way
-                  to earn income while I figured out my “real” path. But as I
-                  stood before my first group of students, something shifted.
-                  Their questions, their struggles, and their small victories
-                  sparked something within me. I wasn’t just imparting
-                  knowledge; I was connecting, guiding, and witnessing growth in
-                  real time. It was a quiet revelation, one that grew louder
-                  with each session. <br />
-                </p>
-              </div>
-              <div className="text-justify">
-                {" "}
-                <p style={{ fontSize: 25, textAlign: "justify" }}>
-                  Embracing teaching wasn’t without its challenges. I had to
-                  learn to navigate diverse learning styles, manage classroom
-                  dynamics, and overcome my own self-doubt. There were moments
-                  when I questioned whether I was cut out for this—nights spent
-                  preparing lessons, worrying if I was making a difference. But
-                  with each hurdle, I grew. I sought mentorship, refined my
-                  methods, and leaned into the discomfort of growth. I
-                  discovered that teaching wasn’t just about delivering
-                  information; it was about fostering curiosity, building
-                  confidence, and creating a space where students could thrive.
-                  The more I invested in my students, the more I realized how
-                  much I was learning from them.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-      {/* end of about me */}
-      {/* start of experience */}
-      <div className="my-4 mx-auto m-5 bg-light mt-5">
-        <div className="row g-0 m-5">
-          <h1 className="text-center">Experience</h1>
-          <div className="col-md-12">
-            <h3 className="text-center">
-              A year of experience as an Instructor on Bohol Island State
-              University
-            </h3>
-          </div>
-          <div className="col-md-6 p-3 rounded">
-            <img src={image1} alt="" className="img-fluid rounded" />
-          </div>
-          <div className="col-md-6 p-3 rounded">
-            <img src={image2} alt="" className="img-fluid rounded" />
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
