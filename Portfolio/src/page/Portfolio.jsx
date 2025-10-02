e8c674576f97c13638c45732b05905e3621ba2db
AaronJaig <128724339+AaronJaig@users.noreply.github.com>
2025-10-02 22:24:20 +0800

    Add Workout project section to portfolio page
    Added a new workout.jpg image asset and updated Portfolio.jsx to include a new 'Workout' project section with image and description. Updated portfolio.css with styles for the new section and adjusted background and layout styles for improved appearance.

--- Portfolio/src/page/Portfolio.jsx
+++ Portfolio/src/page/Portfolio.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import alien from "../assets/alien.png";
 import krabbi from "../assets/krabbi.png";
+import workout from "../assets/workout.jpg";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 function Portfolio() {
@@ -13,22 +14,37 @@ function Portfolio() {
           </div>
         </div>
         <div class="right-pane">
-          <h1>View my Work</h1>
-          <p>
+          <h1>VIEW MY WORK</h1>
+          {/* <p>
             I design intuitive digital experiences that help startups connect
             with their users.
-          </p>
+          </p> */}
         </div>
       </div>
-      <div className="introscreen">
-        <div class="left-pane ">
-          <div className="image-bo">
-            <img src={krabbi} alt="Sample" />
+      <div className="container mt-5 ">
+        <div className="introscreen krabbibackground border border-5 border-dark rounded p-3">
+          <div class="left-pane">
+            <div className="">
+              <img src={krabbi} alt="Sample" />
+            </div>
+          </div>
+          <div class="right-pane">
+            <h1>Krabbi</h1>
+            <p>A Crab Image Recognition App</p>
           </div>
         </div>
-        <div class="right-pane">
-          <h1>Krabbi</h1>
-          <p>A Crab Image Recognition App</p>
+      </div>
+      <div className="container mt-5 ">
+        <div className="introscreen krabbibackground border border-5 border-dark rounded p-3">
+          <div class="left-pane">
+            <div className="workoutimage">
+              <img src={workout} alt="Sample" />
+            </div>
+          </div>
+          <div class="right-pane">
+            <h1>Workout</h1>
+            <p>A Workout Schedule Application</p>
+          </div>
         </div>
       </div>
     </div>
