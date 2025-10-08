f5dcfedcf52b7052887f40fb8590a67e90be4019
AaronJaig <128724339+AaronJaig@users.noreply.github.com>
2025-10-08 18:57:41 +0800

    Update App.jsx

--- Portfolio/src/App.jsx
+++ Portfolio/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <main className="main-content">
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/Portfolio" element={<Home />} />
       </Routes>
       <Footer />
     </main>
